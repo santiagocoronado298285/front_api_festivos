57635898a3c5148ebf01cb8388f7bef574cf9527
scoronadovasco <scoronadovasco@gmail.com>
2025-11-22 09:42:44 -0500

    feat: update Angular dependencies and add Material Design components
    - Added Angular CDK and Material dependencies to package.json.
    - Created a new ReferenciasMaterialModule to encapsulate Material Design modules.
    - Updated app component to import the new ReferenciasMaterialModule.
    - Styled the app with a custom mat-toolbar and added global Material theme.
    - Removed placeholder content from app.html and replaced it with a structured layout using Material components.
    - Added basic styling for the mat-toolbar in app.css.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReferenciasMaterialModule } from '../shared/modules/referencias-material.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    ReferenciasMaterialModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
